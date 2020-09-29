5d7e45fcd63785ee5aa57a0661af13c6f6b7ab20
Adam Powers <apowers@ato.ms>
2020-09-28 19:58:31 -0700

    initial framework

---
+++ index.js
@@ -0,0 +1,8 @@
+const { EventBase, EventBusBase } = require("./lib/EventBase");
+const Perception = require("./lib/Perception");
+
+module.exports = {
+    EventBase,
+    EventBusBase,
+    Perception
+};
