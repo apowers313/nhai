80ba33e573430c2b6e45c5ecd9b33e7bfcb60078
Adam Powers <apowers@ato.ms>
2020-10-03 14:33:05 -0700

    initial logging

---
+++ test/log.js
@@ -0,0 +1,26 @@
+const { Log, Config } = require("../index");
+const assert = require("chai").assert;
+var stdMocks = require("std-mocks");
+
+describe("Log", function() {
+    it("is Function", function() {
+        assert.isFunction(Log);
+    });
+
+    it("has logging functions");
+
+    it("prints to stdout", function() {
+        assert.strictEqual(Config.get("app-name"), "nhai");
+
+        stdMocks.use();
+        Log.info("this is a test");
+        stdMocks.restore();
+
+        let output = stdMocks.flush();
+        assert.strictEqual(output.stdout.length, 1);
+        assert.strictEqual(output.stderr.length, 0);
+        let logObj = JSON.parse(output.stdout[0]);
+        assert.strictEqual(logObj.name, "nhai");
+        assert.strictEqual(logObj.msg, "this is a test");
+    });
+});
