de3e1b125d130b8683af351753010dd5544aaf8e
Adam Powers <apowers@ato.ms>
2020-10-10 14:41:24 -0700

    refactor interface

--- test/component.js
+++ test/component.js
@@ -1,52 +1,56 @@
-const {PerceptionEvent, Component} = require("../index");
+const {Component, EventBase, EventBusBase} = require("../index");
 const {assert} = require("chai");
+
+// helpers
+let testBus;
+
+class TestEvent extends EventBase {
+    get sourceName() {
+        return "dummy";
+    }
+
+    get sourceType() {
+        return "test";
+    }
+
+    get allowedEventTypes() {
+        return new Set(["register", "foo", "bar"]);
+    }
+
+    get eventBus() {
+        return testBus;
+    }
+}
+
+testBus = new EventBusBase(TestEvent);
 
 describe("Component", function() {
     afterEach(function() {
         Component.clearList();
     });
 
-    it("accepts argument inputs", function() {
-        let d = {
-            beer: "mmm",
-        };
-        let c = new Component("foo", "bar", d);
+    it("can construct", function() {
+        let c = new Component("foo", "bar", TestEvent);
         assert.instanceOf(c, Component);
-        assert.strictEqual(c.module, d);
-        assert.strictEqual(c.name, "foo");
-        assert.strictEqual(c.type, "bar");
-    });
-
-    it("accepts event input", function() {
-        let pe = new PerceptionEvent("foo", "bar");
-        pe.data = {};
-        let c = new Component(pe);
-        assert.instanceOf(c, Component);
-        assert.strictEqual(c.module, pe.data);
         assert.strictEqual(c.name, "foo");
         assert.strictEqual(c.type, "bar");
     });
 
     it("throws on bad name", function() {
         assert.throws(() => {
-            new Component(undefined, "type", {data: "foo"});
-        }, TypeError, "expected 'name' to be String while constructing Component");
+            new Component(undefined, "type", TestEvent);
+        }, TypeError, "Component.constructor expected 'name' to be a string, got: undefined");
     });
 
     it("throws on bad type", function() {
         assert.throws(() => {
-            new Component("name", 3, {data: "foo"});
-        }, TypeError, "expected 'type' to be String while constructing Component");
-    });
-
-    it("throws on bad data", function() {
-        assert.throws(() => {
-            new Component("name", "type", undefined);
-        }, TypeError, "expected 'module' to be Object while constructing Component");
+            new Component("name", 3, TestEvent);
+        }, TypeError, "Component.constructor expected 'type' to be a string, got: 3");
     });
 
     it("throws on unrecognized type");
     it("throws on unrecognized module class");
+    it("registers component");
 
     describe("get", function() {
         it("returns a Map", function() {
@@ -70,36 +74,27 @@ describe("Component", function() {
 
     describe("register", function() {
         it("registers module", function() {
-            let c = new Component("myName", "myType", {data: "foo"});
+            let c = new Component("myName", "myType", TestEvent);
             Component.register(c);
             let l = Component.list;
             assert.strictEqual(l.get("myName"), c);
         });
 
-        it("registers based on event", function() {
-            let pe = new PerceptionEvent("foo", "bar");
-            pe.data = {};
-            let c = new Component(pe);
-            Component.register(c);
-            let l = Component.list;
-            assert.strictEqual(l.get("foo"), c);
-        });
-
         it("throws on registering non-Component", function() {
             assert.throws(() => {
                 Component.register({});
-            }, TypeError, "registerModule expected Component argument");
+            }, TypeError, "Component.register expected 'comp' to be instanceof Component, got: Object");
         });
 
         it("throws on duplicate name", function() {
-            new Component("myName", "myType1", {data: "foo"});
+            new Component("myName", "myType1", TestEvent);
             assert.throws(() => {
-                new Component("myName", "myType2", {data: "bar"});
+                new Component("myName", "myType2", TestEvent);
             });
         });
 
         it("allows re-registration of same object", function() {
-            let c = new Component("myName", "myType", {data: "foo"});
+            let c = new Component("myName", "myType", TestEvent);
             Component.register(c);
             Component.register(c);
         });
