c58576e705d7da729296a839439edf85a23a6b05
Adam Powers <apowers@ato.ms>
2020-10-11 12:09:07 -0700

    add random number generator

--- test/utility.js
+++ test/utility.js
@@ -1,7 +1,7 @@
 const {assert} = require("chai");
 
 const {Utility} = require("../index");
-const {checkType, checkInstance} = Utility;
+const {checkType, checkInstance, randomSeed, randomFloat, randomInt} = Utility;
 
 // helpers
 class TestClass {}
@@ -68,6 +68,77 @@ describe("Utility", function() {
         it("allows right instance", function() {
             let testClass = new TestClass();
             checkInstance("myFn", "testClass", testClass, TestClass);
+        });
+    });
+
+    describe("random", function() {
+        it("returns random float", function() {
+            let f = randomFloat();
+            assert.isNumber(f);
+            assert.isFalse(f % 1 === 0);
+        });
+
+        it("returns random int", function() {
+            let i = randomInt();
+            assert.isNumber(i);
+            assert.isTrue(i % 1 === 0);
+        });
+
+        it("returns same sequence for same seed", function() {
+            randomSeed("hello.");
+            assert.strictEqual(randomFloat(), 0.9282578795792454);
+            assert.strictEqual(randomFloat(), 0.3752569768646784);
+            assert.strictEqual(randomFloat(), 0.7316977468919549);
+        });
+
+        it("returns different sequence for different seed", function() {
+            randomSeed("hello!");
+            assert.notStrictEqual(randomFloat(), 0.9282578795792454);
+            assert.notStrictEqual(randomFloat(), 0.3752569768646784);
+            assert.notStrictEqual(randomFloat(), 0.7316977468919549);
+        });
+
+        it("accepts integer seed", function() {
+            randomSeed(3);
+            assert.strictEqual(randomFloat(), 0.7568531143538124);
+            assert.strictEqual(randomFloat(), 0.6138858151827953);
+            assert.strictEqual(randomFloat(), 0.9803335113014127);
+        });
+
+        it("accepts object seed", function() {
+            randomSeed({foo: "bar"});
+            assert.strictEqual(randomFloat(), 0.3799992233952366);
+            assert.strictEqual(randomFloat(), 0.7128710993724056);
+            assert.strictEqual(randomFloat(), 0.6180286446270832);
+        });
+
+        it("accepts date seed", function() {
+            randomSeed(new Date(0));
+            assert.strictEqual(randomFloat(), 0.45849928108785903);
+            assert.strictEqual(randomFloat(), 0.41759607379639296);
+            assert.strictEqual(randomFloat(), 0.9148432874951367);
+        });
+
+        it("undefined seed is non-deterministic", function() {
+            randomSeed();
+            let r1 = randomFloat();
+            let r2 = randomFloat();
+            let r3 = randomFloat();
+            randomSeed();
+            assert.notStrictEqual(randomFloat(), r1);
+            assert.notStrictEqual(randomFloat(), r2);
+            assert.notStrictEqual(randomFloat(), r3);
+        });
+
+        it("null seed is non-deterministic", function() {
+            randomSeed(null);
+            let r1 = randomFloat();
+            let r2 = randomFloat();
+            let r3 = randomFloat();
+            randomSeed(null);
+            assert.notStrictEqual(randomFloat(), r1);
+            assert.notStrictEqual(randomFloat(), r2);
+            assert.notStrictEqual(randomFloat(), r3);
         });
     });
 });
