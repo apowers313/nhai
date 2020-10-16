0ec6ba6d06fff29fee3d6adae327e4859cba47ef
Adam Powers <apowers@ato.ms>
2020-10-16 05:17:47 -0700

    simplify Utility and Log outputs

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const {Config} = require("./lib/Config");
 const {EventBase, EventBusBase, EventFilter, EventListener} = require("./lib/EventBase");
 const {FeatureExtractor} = require("./lib/FeatureExtractor");
 const {Intrinsic} = require("./lib/Intrinsic");
-const {Log} = require("./lib/Log");
+const Log = require("./lib/Log");
 const {Perception, PerceptionEvent, PerceptionModule} = require("./lib/Perception");
 const {Significance, SignificanceEvent} = require("./lib/Significance");
-const {Utility} = require("./lib/Utility");
+const Utility = require("./lib/Utility");
 
 module.exports = {
     Action,
