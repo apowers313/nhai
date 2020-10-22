ca8b708ebefc8480d6f220e3aa5e92367f6b2a25
Adam Powers <apowers@ato.ms>
2020-10-22 13:21:09 -0700

    cleanup

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 const nethackStart = require("@neth4ck/neth4ck");
 const nethackShimCallback = require("./src/nethackCallback");
-// async function nethackShimCallback(name, ... args) {
-//     console.log(`callback: ${name} ${args}`);
-// }
-
-console.log("nethackShimCallback", nethackShimCallback);
 
 nethackStart(nethackShimCallback);
