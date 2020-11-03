040480a3f01d9f6624533985a318afaf6064fb82
Adam Powers <apowers@ato.ms>
2020-11-02 21:23:47 -0800

    add tests

--- test/intrinsic.js
+++ test/intrinsic.js
@@ -4,6 +4,7 @@ const {Intrinsic, Component, Significance} = require("..");
 describe("Intrinsic", function() {
     afterEach(function() {
         Component.clearList();
+        Intrinsic.clearList();
         Significance.eventBus.removeAllListeners();
     });
 
@@ -41,6 +42,13 @@ describe("Intrinsic", function() {
             assert.strictEqual(i.value, 3.14159);
         });
 
+        it("throws on unrecognized string", function() {
+            let i = new Intrinsic("test");
+            assert.throws(() => {
+                i.value = "hi there!";
+            }, TypeError, "Intrinsic.value couldn't parse string as float: 'hi there!'");
+        });
+
         it("can be overloaded", function() {
             const goldValue = "\\G1EBC091E:3";
             const goldRegex = /^\\G[A-F0-9]{8}:(?<goldval>[0-9]*)$/;
@@ -65,10 +73,105 @@ describe("Intrinsic", function() {
             let i = new Intrinsic("test");
             Significance.eventBus.on("change", (e) => {
                 assert.strictEqual(e.type, "change");
-                assert.strictEqual(e.data, 3.14159);
+                assert.isObject(e.data);
+                assert.strictEqual(e.data.oldVal, null);
+                assert.strictEqual(e.data.newVal, 3.14159);
+                assert.strictEqual(e.data.intrinsic, i);
                 done();
             });
             i.value = 3.14159;
         });
+
+        it("doesn't emit change if value is the same", function(done) {
+            let i = new Intrinsic("test");
+            i.value = 3.14159;
+            Significance.eventBus.on("change", () => {
+                assert.fail("should not have emitted change event");
+            });
+            i.value = 3.14159;
+            setTimeout(done, 10);
+        });
+
+        describe("options", function() {
+            describe("min", function() {
+                it("default 0", function() {
+                    let i = new Intrinsic("test");
+                    assert.strictEqual(i.min, 0);
+                });
+
+                it("sets through opts", function() {
+                    let i = new Intrinsic("test", {min: -10});
+                    assert.strictEqual(i.min, -10);
+                });
+
+                it("is read only", function() {
+                    let i = new Intrinsic("test", {min: -10});
+                    assert.strictEqual(i.min, -10);
+                    i.min = 3;
+                    assert.strictEqual(i.min, -10);
+                });
+            });
+
+            describe("max", function() {
+                it("default 0", function() {
+                    let i = new Intrinsic("test");
+                    assert.strictEqual(i.max, 100);
+                });
+
+                it("sets through opts", function() {
+                    let i = new Intrinsic("test", {max: 1000});
+                    assert.strictEqual(i.max, 1000);
+                });
+
+                it("is read only", function() {
+                    let i = new Intrinsic("test", {max: 1000});
+                    assert.strictEqual(i.max, 1000);
+                    i.max = 12;
+                    assert.strictEqual(i.max, 1000);
+                });
+            });
+
+            describe("positive", function() {
+                it("default false", function() {
+                    let i = new Intrinsic("test");
+                    assert.strictEqual(i.positive, false);
+                });
+
+                it("sets through opts", function() {
+                    let i = new Intrinsic("test", {positive: true});
+                    assert.strictEqual(i.positive, true);
+                });
+
+                it("sets through opts (truthy)", function() {
+                    let i = new Intrinsic("test", {positive: 1});
+                    assert.strictEqual(i.positive, true);
+                });
+
+                it("sets through opts (falsy)", function() {
+                    let i = new Intrinsic("test", {positive: 0});
+                    assert.strictEqual(i.positive, false);
+                });
+
+                it("is read only", function() {
+                    let i = new Intrinsic("test", {positive: true});
+                    assert.strictEqual(i.positive, true);
+                    i.positive = false;
+                    assert.strictEqual(i.positive, true);
+                });
+            });
+        });
+
+        // describe("getIntrinsic", function() {
+        //     it("returns intrinsic", function() {
+        //         let i1 = new Intrinsic("bob");
+        //         let i2 = Intrinsic.getIntrinsic("bob");
+        //         assert.strictEqual(i1, i2);
+        //     });
+
+        //     it("returns unknown on not found", function() {
+        //         let i = Intrinsic.getIntrinsic("asdf");
+        //         assert.isUndefined(i);
+        //     });
+        // });
     });
 });
