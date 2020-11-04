2f87dc65dfa3841ea36512d98ff2d9ef62a8ab5f
Adam Powers <apowers@ato.ms>
2020-11-03 21:39:59 -0800

    debug github build

--- test/utility.js
+++ test/utility.js
@@ -118,8 +118,8 @@ describe("Utility", function() {
             assert.strictEqual(randomFloat(), 0.41759607379639296);
             assert.strictEqual(randomFloat(), 0.9148432874951367);
             randomSeed(new Date(0));
-            console.log("Date(0)", new Date(0));
-            console.log("randomoFloat", randomFloat());
+            console.warn("Date(0)", new Date(0));
+            console.warn("randomoFloat", randomFloat());
         });
 
         it("undefined seed is non-deterministic", function() {
