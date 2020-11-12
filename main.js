d59e2c36f438a6393678b55b75f11de60320ef10
Adam Powers <apowers@ato.ms>
2020-11-12 07:17:31 -0800

    feat(nethack): run NetHack in an infinite loop
    fix #31, close #28

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 const nethackShimCallback = require("./src/nethackCallback");
 const {init, Log} = require("./index");
 let nethackInternalLogger;
-/* global WebAssembly */
-const BYTES_PER_KB = 1024;
-const NS_PER_SEC = 1e9;
-const US_PER_SEC = 1e6;
 let runCount = 0;
-let startTime;
-let startUsage;
 
 // this returns an Object containing the emscription Module options
 // the NetHack emscripten implementation adds lots of state information
@@ -16,36 +10,34 @@ let startUsage;
 function buildWasmModule() {
     let {info, warn} = nethackInternalLogger;
     return {
-        logReadFiles: true,
-        noExitRuntime: false,
-        noInitialRun: false,
-        print: info,
-        printErr: warn,
         // undocumented callback function that is run when the emscripten program completes
         quit: function(errorCode) {
-            let cpuUsage = process.cpuUsage(startUsage);
-            let endTime = process.hrtime.bigint();
-            let runTime = endTime - startTime;
-            console.info(`NetHack run # ${runCount} completed with status: ${errorCode} :: Run time: ${ns2s(runTime)}   User CPU: ${cpuUsage.user / US_PER_SEC} sec.   System CPU: ${cpuUsage.system / US_PER_SEC} sec.`);
+            console.info(`NetHack run # ${runCount} completed with status: ${errorCode}`);
+            printTimer();
             // run NetHack in an infinite loop
             runNethack();
         },
+        logReadFiles: true,
+        // noExitRuntime: false,
+        // noInitialRun: false,
+        print: info,
+        printErr: warn,
+        // print: () => {},
+        // printErr: () => {},
     };
 }
 
+// run NetHack
 function runNethack() {
     runCount++;
     console.info(`Starting NetHack: run #${runCount}`);
-    let {rss, heapTotal, heapUsed, external, arrayBuffers} = process.memoryUsage();
-    let percentHeap = (heapUsed / heapTotal) * 100;
-    console.debug(`Heap Usage: ${b2s(heapUsed)} (${percentHeap.toFixed(1)}% of ${b2s(heapTotal)}) :: ArrayBuffers: ${b2s(arrayBuffers)}   External: ${b2s(external)}   [[ Total Memory (RSS): ${b2s(rss)} ]]`);
-    clearRequireCache();
+    printMemory();
     let nethackStart = require("@neth4ck/neth4ck");
     setTimeout(() => {
-        startUsage = process.cpuUsage();
-        startTime = process.hrtime.bigint();
+        startTimer();
         nethackStart(nethackShimCallback, buildWasmModule());
         process.removeAllListeners("uncaughtException");
+        process.removeAllListeners("unhandledRejection");
         process.on("uncaughtException", function(ex) {
             console.warn("uncaughtException (but it's okay)");
             // TODO: check if message is "unreachable" and top of stack is "at nh_terminate (<anonymous>:wasm-function[974]:0x13abdd)"
@@ -54,39 +46,64 @@ function runNethack() {
                 throw ex;
             }
         });
-    }, 0);
+    }, 7000);
 }
 
-// # bytes to string
-function b2s(b) {
-    let size = 0;
+let startTime;
+let startUsage;
+function startTimer() {
+    startUsage = process.cpuUsage();
+    startTime = process.hrtime.bigint();
+}
 
-    while (b > BYTES_PER_KB) {
-        b = b / BYTES_PER_KB;
-        size++;
+// print time since startTimer
+function printTimer() {
+    const NS_PER_SEC = 1e9;
+    const US_PER_SEC = 1e6;
+
+    let cpuUsage = process.cpuUsage(startUsage);
+    let endTime = process.hrtime.bigint();
+    let runTime = endTime - startTime;
+    console.debug(`Run time: ${ns2s(runTime)}   User CPU: ${cpuUsage.user / US_PER_SEC} sec.   System CPU: ${cpuUsage.system / US_PER_SEC} sec.`);
+
+    // nanoseconds to seconds string
+    function ns2s(ns) {
+        let s = BigInt(ns) / BigInt(NS_PER_SEC);
+        ns = ns - (BigInt(ns) / BigInt(NS_PER_SEC));
+        return `${s}.${pad(ns)} sec.`;
+        // return `${s} sec.`;
     }
 
-    switch (size) {
-    case 0: return `${b.toFixed(2)} B`;
-    case 1: return `${b.toFixed(2)} KB`;
-    case 2: return `${b.toFixed(2)} MB`;
-    case 3: return `${b.toFixed(2)} GB`;
-    case 4: return `${b.toFixed(2)} TB`;
-    default: return `${b.toFixed(2)} ??`;
+    function pad(num) {
+        let s = `000000000${num.toString()}`;
+        return s.substr(s.length - 9);
     }
 }
 
-// nanoseconds to seconds string
-function ns2s(ns) {
-    let s = BigInt(ns) / BigInt(NS_PER_SEC);
-    // ns = ns - (BigInt(ns) / BigInt(NS_PER_SEC));
-    // return `${s}.${ns} sec.`;
-    return `${s} sec.`;
-}
+// print memory usage
+function printMemory() {
+    let {rss, heapTotal, heapUsed, external, arrayBuffers} = process.memoryUsage();
+    let percentHeap = (heapUsed / heapTotal) * 100;
+    console.debug(`Heap Usage: ${b2s(heapUsed)} (${percentHeap.toFixed(1)}% of ${b2s(heapTotal)}) :: ArrayBuffers: ${b2s(arrayBuffers)}   External: ${b2s(external)}   [[ Total Memory (RSS): ${b2s(rss)} ]]`);
 
-function clearRequireCache() {
-    for (let key of Object.keys(require.cache)) {
-        delete require.cache[key];
+    // # bytes to string
+    function b2s(b) {
+        const BYTES_PER_KB = 1024;
+        let size = 0;
+
+        while (b > BYTES_PER_KB) {
+            b = b / BYTES_PER_KB;
+            size++;
+        }
+
+        switch (size) {
+        case 0: return `${b.toFixed(2)} B`;
+        case 1: return `${b.toFixed(2)} KB`;
+        case 2: return `${b.toFixed(2)} MB`;
+        case 3: return `${b.toFixed(2)} GB`;
+        case 4: return `${b.toFixed(2)} TB`;
+        default: return `${b.toFixed(2)} ??`;
+        }
     }
 }
 
