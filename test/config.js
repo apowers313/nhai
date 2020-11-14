56e1939aec944dc4598bd7dc0a54940b49851a81
Adam Powers <apowers@ato.ms>
2020-11-14 13:18:01 -0800

    test(config): test YAML config file loading
    New test to ensure loading YAML config files works. Should also be OS independent.
    
    fix #24

--- test/config.js
+++ test/config.js
@@ -1,5 +1,17 @@
 const {assert} = require("chai");
 const {Config} = require("..");
+const fs = require("fs");
+const path = require("path");
+
+// helpers
+function copyFile(srcPath, dstPath) {
+    let data = fs.readFileSync(srcPath);
+    fs.writeFileSync(dstPath, data);
+}
+
+function deleteFile(path) {
+    fs.unlinkSync(path);
+}
 
 describe("config", function() {
     it("is class", function() {
@@ -31,6 +43,38 @@ describe("config", function() {
         it("has version", function() {
             let {version} = require("../package.json");
             assert.strictEqual(Config.getConfig().get("app-version"), version);
+        });
+    });
+
+    describe("init", function() {
+        describe("YAML", function() {
+            beforeEach(function() {
+                copyFile(path.join(__dirname, "helpers", "config.yml"), path.join(process.cwd(), ".nhairc"));
+            });
+
+            afterEach(function() {
+                deleteFile(path.join(process.cwd(), ".nhairc"));
+            });
+
+            it("loads file", async function() {
+                await Config.init();
+                assert.strictEqual(Config.get("test"), "yaml-cookie-yummy");
+            });
+
+            it("preserves defaults", async function() {
+                await Config.init();
+                assert.strictEqual(Config.get("app-version"), require("../package.json").version);
+            });
+
+            it("sets configFileList", async function() {
+                await Config.init();
+                assert.deepEqual(Config.configFileList, [
+                    {
+                        config: {test: "yaml-cookie-yummy"},
+                        filepath: "/Users/ampower/Projects/personal/nhai/.nhairc",
+                    },
+                ]);
+            });
         });
     });
 });
