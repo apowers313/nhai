b840c0f163d57a380aea5557f3e34df078dd6a63
Adam Powers <apowers@ato.ms>
2021-04-25 16:54:35 -0700

    chore: ignore linting of handlebars files

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
     parserOptions: {
     	"project": "./tsconfig.json"
     },
+    ignorePatterns: [
+        "*.hbs"
+    ],
     plugins: [
         "@typescript-eslint",
     ],
