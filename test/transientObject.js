4e57d44df1c94f89f5c7ff6af9ac29a15c0b4329
Adam Powers <apowers@ato.ms>
2021-10-18 22:58:56 -0700

    feat: allow extending properties on base transient object

--- test/transientObject.js
+++ test/transientObject.js
@@ -6,6 +6,32 @@ describe("TransientObject", function() {
         assert.isFunction(TransientObject);
     });
 
+    describe("options", function() {
+        describe("id", function() {
+            it("sets id", function() {
+                let to = new TransientObject({id: 123});
+                assert.strictEqual(to.id, 123);
+            });
+        });
+
+        describe("props", function() {
+            it("sets all props", function() {
+                let to = new TransientObject({props: ["foo", "bar"]});
+                // eslint-disable-next-line no-prototype-builtins
+                assert.isTrue(to.hasOwnProperty("foo"));
+                // eslint-disable-next-line no-prototype-builtins
+                assert.isTrue(to.hasOwnProperty("foo"));
+                to.foo = 1;
+                to.bar = "beer";
+                assert.strictEqual(to.foo, 1);
+                assert.strictEqual(to.bar, "beer");
+                assert.isUndefined(to.data.foo);
+                assert.isUndefined(to.data.bar);
+                assert.deepEqual(to.toString(), "{}");
+            });
+        });
+    });
+
     describe("Proxy", function() {
         describe("getter", function() {
             it("throws if not loaded", function() {
